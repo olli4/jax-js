1a119b85a34a582f04c57e0a762a8ff3695d16a6
Eric Zhang <ekzhang1@gmail.com>
2025-08-03 20:17:34 -0400

    Implement conGeneralDilated

--- src/lax.ts
+++ src/lax.ts
@@ -4,9 +4,109 @@
 // set of operations, as they both build open the same foundations.
 
 import { Array } from "./frontend/array";
-import { conv } from "./frontend/core";
+import { conv as convPrimitive } from "./frontend/core";
+import { rep, zipn } from "./utils";
 
-export function convGeneralDilated(lhs: Array, rhs: Array): Array {
-  // XXX: Pass down padding and the other parameters.
-  return conv(lhs, rhs) as Array;
+type PaddingType = "VALID" | "SAME" | "SAME_LOWER" | [number, number][];
+
+function padtypeToPads(
+  inShape: number[],
+  filterShape: number[],
+  strides: number[],
+  dilation: number[],
+  padding: string,
+): [number, number][] {
+  const padType = padding.toUpperCase();
+  switch (padType) {
+    case "VALID":
+      return rep<[number, number]>(inShape.length, [0, 0]);
+    case "SAME":
+    case "SAME_LOWER": {
+      const outShape = inShape.map((size, i) => Math.ceil(size / strides[i]));
+      const padSizes = zipn(
+        outShape,
+        strides,
+        filterShape,
+        dilation,
+        inShape,
+      ).map(([o, s, k, d, i]) =>
+        Math.max(0, (o - 1) * s + 1 + (k - 1) * d - i),
+      );
+      if (padType === "SAME") {
+        return padSizes.map((size) => [size >> 1, size - (size >> 1)]);
+      } else {
+        return padSizes.map((size) => [size - (size >> 1), size >> 1]);
+      }
+    }
+    default:
+      throw new Error(`Unknown padding type: ${padType}`);
+  }
+}
+
+/**
+ * General n-dimensional convolution operator, with optional dilation.
+ *
+ * The semantics of this operation mimic the `jax.lax.conv_general_dilated`
+ * function in JAX, which wraps XLA's general convolution operator.
+ *
+ * Grouped convolutions are not supported right now.
+ */
+export function convGeneralDilated(
+  lhs: Array,
+  rhs: Array,
+  windowStrides: number[],
+  padding: PaddingType,
+  {
+    lhsDilation,
+    rhsDilation,
+  }: {
+    lhsDilation?: number[];
+    rhsDilation?: number[];
+  } = {},
+): Array {
+  if (typeof padding === "string") {
+    if (lhsDilation?.some((d) => d !== 1)) {
+      throw new Error(
+        "String padding is not supported for transposed convolutions",
+      );
+    }
+    padding = padtypeToPads(
+      lhs.shape.slice(2),
+      rhs.shape.slice(2),
+      windowStrides,
+      rhsDilation ?? rep(rhs.ndim - 2, 1),
+      padding,
+    );
+  }
+  return convPrimitive(lhs, rhs, {
+    strides: windowStrides,
+    padding,
+    lhsDilation,
+    rhsDilation,
+  }) as Array;
+}
+
+/** Convenience wrapper around `convGeneralDilated`. */
+export function convWithGeneralPadding(
+  lhs: Array,
+  rhs: Array,
+  windowStrides: number[],
+  padding: PaddingType,
+  lhsDilation?: number[],
+  rhsDilation?: number[],
+) {
+  return convGeneralDilated(lhs, rhs, windowStrides, padding, {
+    lhsDilation,
+    rhsDilation,
+  });
+}
+
+/** Convenience wrapper around `convGeneralDilated`. */
+export function conv(
+  lhs: Array,
+  rhs: Array,
+  windowStrides: number[],
+  padding: PaddingType,
+) {
+  return convGeneralDilated(lhs, rhs, windowStrides, padding);
 }
--- test/conv.test.ts
+++ test/conv.test.ts
@@ -15,7 +15,19 @@ suite.each(devices)("device:%s", (device) => {
   test("1d convolution", () => {
     const x = np.array([[[1, 2, 3, 4, 5]]]);
     const y = np.array([[[2, 0.5, -1]]]);
-    const result = lax.convGeneralDilated(x, y);
+    const result = lax.convGeneralDilated(x.ref, y.ref, [1], "VALID");
     expect(result.js()).toEqual([[[0, 1.5, 3]]]);
+
+    const result2 = lax.convGeneralDilated(x, y, [1], "SAME");
+    expect(result2.js()).toEqual([[[-1.5, 0, 1.5, 3, 10.5]]]);
+  });
+
+  test("padding 'SAME' and 'SAME_LOWER'", () => {
+    const x = np.ones([1, 1, 5]);
+    const y = np.ones([1, 1, 2]);
+    const resultSame = lax.convGeneralDilated(x.ref, y.ref, [1], "SAME");
+    expect(resultSame.slice(0, 0).js()).toEqual([2, 2, 2, 2, 1]);
+    const resultSameLower = lax.convGeneralDilated(x, y, [1], "SAME_LOWER");
+    expect(resultSameLower.slice(0, 0).js()).toEqual([1, 2, 2, 2, 2]);
   });
 });
